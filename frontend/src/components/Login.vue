b6f83f11be1f198653a86ac304418599b73843fa
JangHyunjeong <85441226+JangHyunjeong@users.noreply.github.com>
2023-06-30 20:54:00 +0900

    💄 login : join과 스타일 구조 맞춤

--- frontend/src/components/Login.vue
+++ frontend/src/components/Login.vue
@@ -1,27 +1,43 @@
 <template>
   <div class="join">
     <Logo />
-    <label for="userId" class="label-title">아이디</label>
     <form id="loginForm">
-      <input
-        type="text"
-        class="input-basic h50"
-        name="user_id"
-        placeholder="아이디를 입력해주세요"
-        @input="getLoginId($event.target.value)"
-        maxlength="12"
-      />
+      <dl class="form-group">
+        <dt class="form-title">
+          <label for="userId" class="label-title">아이디</label>
+        </dt>
 
-      <label for="nickName" class="label-title">비밀번호</label>
-      <input
-        type="password"
-        name="user_pw"
-        class="input-basic h50"
-        @input="getLoginPw($event.target.value)"
-        placeholder="비밀번호를 입력해주세요"
-        maxlength="20"
-        autocomplete
-      />
+        <dd class="form-data">
+          <div class="input-div">
+            <input
+              type="text"
+              class="input-basic h50"
+              name="user_id"
+              placeholder="아이디를 입력해주세요"
+              @input="getLoginId($event.target.value)"
+              maxlength="12"
+            />
+          </div>
+        </dd>
+
+        <dt class="form-title">
+          <label for="nickName" class="label-title">비밀번호</label>
+        </dt>
+
+        <dd class="form-data">
+          <div class="input-div">
+            <input
+              type="password"
+              name="user_pw"
+              class="input-basic h50"
+              @input="getLoginPw($event.target.value)"
+              placeholder="비밀번호를 입력해주세요"
+              maxlength="20"
+              autocomplete
+            />
+          </div>
+        </dd>
+      </dl>
     </form>
 
     <div class="induce-join">
